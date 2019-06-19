6ca13af4398819476fdaadb207d095bde4e9c063
Karsten Rabe <karstenrabe91@gmail.com>
2019-06-18 21:33:16 -0400

    Add control flow, generate command

--- cli.js
+++ cli.js
@@ -7,21 +7,41 @@ const execDir = process.cwd();
 
 let config = {};
 
+console.log(`Welcome to React CLI!\nLoading configs...\n`);
+
 // Look for a user config, if not found load defaults
 fs.readFile(`${execDir}/.rc.config.json`, 'utf8', (err, data) => {
 	if (err && err.code === 'ENOENT') {
-		console.log(`Loading default configuration...\n`);
+		console.log(`Default configuration...\n`);
 		fs.readFile(`${__dirname}/.rc.config.json`, 'utf8', (err, data) => {
 			if (err) {
 				console.log(`Error loading default configuration...`, err);
 			}
 			config = JSON.parse(data);
-			console.log(config);
+			main();
 		});
 	}
 	else if (!err) {
-		console.log(`Loading user defined configuration... \n`);
+		console.log(`User defined configuration... \n`);
 		config = JSON.parse(data);
-		console.log(config);
+		main();
 	}
 });
+
+const main = () => {
+	console.log(config, `\n`);
+	if (args[0]) {
+		switch (args[0]) {
+			case 'g':
+				console.log('Generate invoked...');
+				break;
+
+			default:
+				console.log(`Unknown command ${args[0]}`);
+				break;
+		}
+	}
+	else {
+		console.log('Please specify a command to run.');
+	}
+}
