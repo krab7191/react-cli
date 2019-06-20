47be6b277d663e39ef6c3815b920e59d38ef2489
Karsten Rabe <karstenrabe91@gmail.com>
2019-06-20 00:18:41 -0400

    Display asci art

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const chalk = require('chalk');
+const log = console.log;
+
+const art = fs.readFileSync(__dirname + '/../asciArt.txt', 'utf8');
 
 // Inquirer functionality for generating config file
 module.exports = {
 	main: function (str) {
-		fs.readFile(`${str}/package.json`, 'utf8', function (err, data) {
+		fs.readFile(`${str} /package.json`, 'utf8', function (err, data) {
 			if (err) {
 				if (err.code === 'ENOENT') {
 					inquirer
@@ -27,11 +31,14 @@ module.exports = {
 						});
 				}
 			}
-			console.log();
+			else {
+				initConfig();
+			}
 		});
 	}
 };
 
 const initConfig = () => {
-	console.log('Begin asking the user some questions...');
+	log(chalk.black.bgCyan(art));
+	// console.log(asciArt);
 }
